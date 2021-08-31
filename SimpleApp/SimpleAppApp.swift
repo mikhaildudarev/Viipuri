8535205ceb5456bf6b34d245cb0ebd1f8212e945
Mikhail Dudarev <github@dev.mikhaildudarev.com>
2021-08-31 18:38:44 +0300

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Mikhail Dudarev on 31.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
